5ed6b3533159f2f633409eb8b912ca4e1db93d47
Valen2525 <juroseror@unal.edu.co>
2024-11-24 11:52:16 -0500

    taller 4

--- src/main/java/classroom/Asignatura.java
+++ src/main/java/classroom/Asignatura.java
@@ -12,13 +12,13 @@ public class Asignatura {
         this("Sin nombre",Tipo.FUNDAMENTACION);
     }
 
-    public Asignatura(int codigoInterno,Tipo tipo) {
-        this("Sin nombre", codigoInterno, 0,tipo);
-    }
-
-    //public Asignatura(int codigoExterno, Tipo tipo) {
-    //    this("Sin nombre", 0, codigoExterno,tipo);
+    //public Asignatura(int codigoInterno,Tipo tipo) {
+    //    this("Sin nombre", codigoInterno, 0,tipo);
     //}
+
+    public Asignatura(int codigoExterno, Tipo tipo) {
+        this("Sin nombre", 0, codigoExterno,tipo);
+    }
 
     public Asignatura(String nombre, Tipo tipo) {
         this(nombre, 0, 0,tipo);
@@ -38,13 +38,13 @@ public class Asignatura {
         this.tipo = tipo;
     }
 
-    public void cambiarDatos(int codigoInterno) {
-        this.codigoInterno = codigoInterno;
-    }
-
-    //public void cambiarDatos(int codigoExterno) {
-    //    this.codigoExterno = codigoExterno;
+    //public void cambiarDatos(int codigoInterno) {
+    //    this.codigoInterno = codigoInterno;
     //}
+
+    public void cambiarDatos(int codigoExterno) {
+        this.codigoExterno = codigoExterno;
+    }
 
     public void cambiarDatos(String nombre) {
         this.nombre = nombre;
